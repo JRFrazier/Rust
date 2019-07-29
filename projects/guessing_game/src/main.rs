c007c2dcbab10ec82cd8047387044f15bbf5a75d
JRFrazier <jrfrazier21@gmail.com>
2019-07-29 07:35:11 -0400

    added code to guessing game

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,7 +1,14 @@
 use std::io;
+use std::cmp.Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Gues the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is : {}", secret_number);
+
     println!("Please input your guess.");
 
     let mut guess = String::new(); // declares a new "mutable" var called "guess" with an empty instance of a string
@@ -11,4 +18,10 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
